601134aa04cce9f07496dd5e271ce54881bf1e1f
hasalica <83381777+hakancansu@users.noreply.github.com>
2022-05-29 22:20:53 +0300

    Lnaguages/en added

---
+++ src/languages/en.js
@@ -0,0 +1,16 @@
+/* eslint-disable quote-props */
+
+export const en = {
+  'Settings': 'Settings',
+  'Offline Forms': 'Offline Forms',
+  'tab_Menu': 'Menu',
+  'language': 'language',
+  'picture and camera settings': 'picture and camera settings',
+  'device UUID': 'device UUID',
+  'welcome': 'welcome',
+  'English': 'English',
+  'Turkish': 'Turkish',
+  'Form': 'Form',
+  'OfflineForms': 'OfflineForms',
+  'Setting': 'Setting',
+};
