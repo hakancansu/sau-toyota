6e1cb23993ce06c93e332ed23fab8081ce520152
hasalica <83381777+hakancansu@users.noreply.github.com>
2022-05-29 22:21:05 +0300

    Create tr.js
    Languages/tr added

---
+++ src/languages/tr.js
@@ -0,0 +1,15 @@
+/* eslint-disable quote-props */
+
+export const tr = {
+  'Settings': 'Ayarlar',
+  'Offline Forms': 'Çevrimdışı formlar',
+  'tab_Menu': 'Menü',
+  'language': 'dil',
+  'picture and camera settings': 'resim ve kamera ayarları',
+  'welcome': 'hoşgeldin',
+  'English': 'İngilizce',
+  'Turkish': 'Türkçe',
+  'Form': 'Form',
+  'OfflineForms': 'Çevrimdışı form',
+  'Setting': 'Ayarlar',
+};
