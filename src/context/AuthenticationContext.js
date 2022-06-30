2034a203217f1d279514b6cd120ded19f9bc161f
hasalica <83381777+hakancansu@users.noreply.github.com>
2022-06-30 21:59:43 +0300

    AuthenticationContext added

--- src/context/AuthenticationContext.js
+++ src/context/AuthenticationContext.js
@@ -5,8 +5,13 @@ const AuthenticationContext = createContext();
 const AuthenticationProvider = ({ children }) => {
   const [isAuthenticated, setAuthenticated] = useState(false);
   const [name, setName] = useState('');
+
+  const value = React.useMemo(() => ({
+    isAuthenticated, setAuthenticated, name, setName,
+  }), [isAuthenticated, name]);
+
   return (
-    <AuthenticationContext.Provider value={{ isAuthenticated, setAuthenticated, name, setName }}>
+    <AuthenticationContext.Provider value={value}>
       {children}
     </AuthenticationContext.Provider>
   );
--- src/context/LoaderContext.js
+++ src/context/LoaderContext.js
@@ -5,8 +5,13 @@ const LoaderContext = createContext();
 
 const LoaderProvider = ({ children }) => {
   const [loader, setLoader] = useState(false);
+
+  const value = React.useMemo(() => ({
+    setLoader, loader,
+  }), [loader]);
+
   return (
-    <LoaderContext.Provider value={{ setLoader, loader }}>
+    <LoaderContext.Provider value={value}>
       <ModalComponent animationType="none" visible={loader} />
       {children}
     </LoaderContext.Provider>
