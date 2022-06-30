0a6d4344703c015881e3ddb4a61c3595d978749c
hasalica <83381777+hakancansu@users.noreply.github.com>
2022-06-30 22:00:36 +0300

    LocalizeContext edited

--- src/context/LocalizeContext.js
+++ src/context/LocalizeContext.js
@@ -1,7 +1,6 @@
 import React, { createContext, useState } from 'react';
 import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
-import * as SecureStore from 'expo-secure-store';
 import { en } from '../languages/en';
 import { tr } from '../languages/tr';
 
@@ -15,20 +14,16 @@ i18n.defaultSeparator = '|';
 const LocalizeContext = createContext();
 
 const LocalizeProvider = ({ children }) => {
-  const [locale, setLocale] = useState('tr');
+  const [locale, setLocale] = useState(Localization.locale);
+  i18n.locale = locale;
 
-  const getLanguage = async () => {
-    const dil = await SecureStore.getItemAsync('language');
-    return dil;
-  };
-
-  React.useEffect(() => {
-    SecureStore.getItemAsync('language').then(x => { i18n.locale = x; });
-  }, []);
+  const value = React.useMemo(() => ({
+    locale, setLocale,
+  }), [locale]);
 
   return (
-    <LocalizeContext.Provider value={{ i18n, locale, setLocale }}>
-      { children }
+    <LocalizeContext.Provider value={value}>
+      {children}
     </LocalizeContext.Provider>
   );
 };
