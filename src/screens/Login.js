649fec8ebadba63c1306c1f774af8f12c3425e68
hasalica <83381777+hakancansu@users.noreply.github.com>
2022-06-30 22:02:12 +0300

    Login & Menu edited

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,47 +1,117 @@
 import React, { useContext, useState } from 'react';
-import { View, Text, Platform, StyleSheet } from 'react-native';
+import { View, Text, Platform, TextInput, Image, TouchableOpacity } from 'react-native';
 import * as Application from 'expo-application';
 import { Entypo } from '@expo/vector-icons';
+import i18n from 'i18n-js';
+import { LinearGradient } from 'expo-linear-gradient';
+import * as Localization from 'expo-localization';
 import LoginButton from '../components/LoginButton';
-import CustomTextInput from '../components/CustomTextInput';
+import { LoaderContext } from '../context/LoaderContext';
 import { AuthenticationContext } from '../context/AuthenticationContext';
 import { NetworkInfoContext } from '../context/NetworkInfoContext';
+import Members from '../constants/Members.json';
+import { LocalizeContext } from '../context/LocalizeContext';
 
 const Login = () => {
-  const { isAuthenticated, setAuthenticated, name, setName } = useContext(AuthenticationContext);
+  const { setAuthenticated, name, setName } = useContext(AuthenticationContext);
+  const { setLocale, locale } = useContext(LocalizeContext);
+
   const { connect } = useContext(NetworkInfoContext);
-  const [pass, setPass] = useState('');
+  const { setLoader } = useContext(LoaderContext);
+  const [pass, setPass] = useState('Cansu');
+  const [messageError, setMessageError] = useState('');
+  const { Persons } = Members;
 
   const loginHandle = () => {
-    setAuthenticated(true);
-    setName(name);
-    console.log(name);
+    const user = Persons.find(person => (person.name === name && person.surname === pass));
+    if (user !== undefined) {
+      setLoader(true);
+      setTimeout(() => {
+        setLoader(false);
+        setAuthenticated(true);
+      }, (3000));
+    } else {
+      setMessageError('Kullanıcı adınızı veya şifrenizi yanlış girdiniz');
+    }
+
     console.log(connect);
   };
-
   const Uuid = Platform.OS !== 'ios' && Application.androidId;
   return (
-    <View style={{ backgroundColor: '#1E324D', flex: 1 }}>
-      <View style={{ marginTop: '50%', marginHorizontal: '10%', backgroundColor: '#0049C9', padding: 20, borderRadius: 7 }}>
-        <Entypo name="network" size={24} color={connect ? 'green' : 'red'} />
-        <Text style={{ color: 'aliceblue' }}>type your username</Text>
-        {/* <CustomTextInput onChangeText={e => setName(e)} value={name} style={{ marginTop: 0, borderRadius: 10, backgroundColor: 'aliceblue' }} />
-        <Text style={{ color: 'aliceblue' }}>type your surname</Text>
-        <CustomTextInput onChangeText={e => setPass(e)} value={pass} style={{ marginTop: 0, borderRadius: 10, backgroundColor: 'aliceblue' }} /> */}
-        <Text style={{ color: 'white', marginTop: 10 }}>unique id:{ Uuid }</Text>
-        <LoginButton onPress={() => loginHandle()} style={{ backgroundColor: 'aliceblue', marginTop: 30, borderRadius: 10 }} text="Log in" />
+    <LinearGradient colors={['#74b49b', '#f9f8eb']} style={{ flex: 1, padding: 20 }}>
+
+      <View style={{ flexDirection: 'column',
+        justifyContent: 'center',
+        marginTop: '40%',
+        marginHorizontal: '12%' }}
+      >
+        <Text style={{ color: 'aliceblue',
+          fontSize: 50,
+          marginBottom: 15,
+          textAlign: 'center',
+          fontStyle: 'italic' }}
+        >{i18n.t('Hello')} !
+        </Text>
+        <TextInput
+          placeholder={i18n.t('type your username')}
+          onChangeText={e => setName(e)}
+          value={name}
+          style={{ backgroundColor: '#FFFFFF',
+            justifyContent: 'center',
+            paddingHorizontal: 10,
+            height: 40,
+            marginTop: 20,
+            marginBottom: 10,
+            borderRadius: 10 }}
+        />
+        <TextInput
+          placeholder={i18n.t('type your surname')}
+          onChangeText={e => setPass(e)}
+          value={pass}
+          style={{ backgroundColor: '#FFFFFF',
+            justifyContent: 'center',
+            paddingHorizontal: 10,
+            marginBottom: 10,
+            height: 40,
+            marginTop: 20,
+            borderRadius: 10 }}
+        />
+        <Text style={{ color: 'white', marginTop: 10 }}>{i18n.t('unique id')}:{ Uuid }</Text>
+        <Text style={{ color: 'red' }}>{messageError}</Text>
+        <LoginButton
+          onPress={() => { loginHandle(); }}
+          style={{ backgroundColor: 'aliceblue', marginTop: 30, borderRadius: 10 }}
+          text={i18n.t('Log in')}
+        />
+        <Entypo
+          style={{ textAlign: 'center', marginTop: 30 }}
+          name="network"
+          size={34}
+          color={connect ? '#1CE043' : 'red'}
+        />
+        <Text style={{ textAlign: 'center',
+          fontSize: 18,
+          fontStyle:
+         'italic',
+          color: connect ? 'white' : 'white' }}
+        >{connect ? 'Online mod' : 'Offline mod'}
+        </Text>
+        {
+          Localization.locale === locale ? (
+            <TouchableOpacity onPress={() => setLocale('En-US')}>
+              <Image source={require('../../image/Turkey.png')} style={{ width: 42, height: 30 }} />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity onPress={() => setLocale('tr-TR')}>
+              <Image source={require('../../image/England.png')} style={{ width: 42, height: 30 }} />
+            </TouchableOpacity>
+          )
+        }
+
       </View>
-    </View>
+
+    </LinearGradient>
   );
 };
 
 export default Login;
-
-const styles = StyleSheet.create({
-  net: {
-    color: 'red',
-  },
-  nonet: {
-    color: 'white',
-  },
-});
--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,12 +1,42 @@
-import React, { useContext } from 'react';
-import { Text } from 'react-native';
-import i18n from 'i18n-js';
+import I18n from 'i18n-js';
+import React, { useContext, useState, useEffect } from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 import { AuthenticationContext } from '../context/AuthenticationContext';
+import { NetworkInfoContext } from '../context/NetworkInfoContext';
 
 const Menu = () => {
+  const { setAuthenticated } = useContext(AuthenticationContext);
+  const { connect } = useContext(NetworkInfoContext);
   const { name } = useContext(AuthenticationContext);
+  const [netİnfo, setNetİnfo] = useState('');
+
+  useEffect(() => {
+    if (connect === false) {
+      setNetİnfo('çevrimdışı moddasınız');
+    }
+  }, [netİnfo]);
+
   return (
-    <Text style={{ marginTop: 100 }}>{i18n.t('welcome')} {name} </Text>
+    <LinearGradient colors={['#74b49b', '#f9f8eb']} style={{ flex: 1, alignItems: 'center' }}>
+      <MaterialCommunityIcons name="cpu-32-bit" size={300} color="black" style={{ marginTop: '8%' }} />
+      <Text style={{ marginTop: 50, color: '#1e2022', fontSize: 30 }}>{I18n.t('Welcome')} {name}
+      </Text><Entypo name="network" size={24} color={connect ? 'green' : 'red'} />
+      <Text style={{ fontSize: 14, color: 'white' }}>{netİnfo}</Text>
+      <TouchableOpacity
+        onPress={() => setAuthenticated(false)}
+        style={{ backgroundColor: 'aliceblue',
+          marginTop: 30,
+          borderRadius: 10,
+          height: 40,
+          padding: 10,
+          width: 150,
+          alignItems: 'center' }}
+      >
+        <Text style={{ color: '#1e2022' }} onPress={() => setAuthenticated(false)}>{I18n.t('Exit')}</Text>
+      </TouchableOpacity>
+    </LinearGradient>
   );
 };
 
